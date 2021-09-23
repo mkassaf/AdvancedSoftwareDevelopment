8077428ace22524e3821b28d42d425ce365ae2b0
Mustafa Assaf <mustafakassaf@gmail.com>
2021-09-23 17:17:27 +0300

    Add

---
+++ Design Pattern/Singleton/src/edu/najah/cap/Connection.java
@@ -0,0 +1,35 @@
+package edu.najah.cap;
+
+import java.util.ArrayList;
+
+public class Connection {
+
+	public static final String TELNET = null;
+	public static final String SSH = null;
+	public static final String HTTP = null;
+	public static final String SCP = null;
+	public static final String FTP = null;
+
+	public static Connection getInstance(String telnet2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static boolean release(String telnet2) {
+		return false;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static ArrayList<String> getCurrentConnections() {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void send(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- Design Pattern/Singleton/src/edu/najah/cap/SingletonDemo.java
+++ Design Pattern/Singleton/src/edu/najah/cap/SingletonDemo.java
@@ -1,5 +1,9 @@
 package edu.najah.cap;
 
+import java.util.ArrayList;
+
+import com.sun.tools.javac.util.List;
+
 public class SingletonDemo {
 
 	public static void main(String[] args) {
@@ -13,6 +17,39 @@ public class SingletonDemo {
 		System.out.println("id = " + instance2.id); //5 
 		System.out.println("id = " + instance1.id); //5 
 		System.out.println("check is equal : " + (instance1 == instance2)); //true
+		
+		
+		// will create a new object and return a TELNET connection
+		Connection telnetConnection = Connection.getInstance(Connection.TELNET); 
+		// will return the current TELNET connection 
+		Connection telnetConnection2 = Connection.getInstance(Connection.TELNET); 
+		// will create a new object
+		Connection sshConnection = Connection.getInstance(Connection.SSH); 
+		
+		// will create a new object
+		Connection httpConnection = Connection.getInstance(Connection.HTTP); 
+		
+		// will throw an expectation telling the user he can’t create more than 3 connections!
+		Connection scpConnection = Connection.getInstance(Connection.SCP); 
+		
+		// will return the current SSH connection
+		Connection sshConnection2 = Connection.getInstance(Connection.HTTP); 
+
+		// will delete the current TELNET connection and return true, 
+		//and it will return false if there is no TELNET connection at all. 
+		boolean isReleased = Connection.release(Connection.TELNET);
+
+		// will create a new FTP connection and return it.
+		Connection ftpConnection = Connection.getInstance(Connection.FTP); 
+
+		// return a list of current connections as Connection objects
+		ArrayList<String> currentConnections = Connection.getCurrentConnections(); 
+		
+		//Will print a message says: Sending [My message] vi FTP protocol. 
+		ftpConnection.send("My message");
+		//Will print a message says: Sending [My message] vi SSH protocol. 
+		sshConnection.send("My message");
+		
 
 	}
 
