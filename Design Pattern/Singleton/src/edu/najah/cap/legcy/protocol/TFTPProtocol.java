94d042d58d87a5bd66a1dc3e983f519b6ea36088
Mustafa Assaf <mustafakassaf@gmail.com>
2021-09-27 08:43:00 +0300

    Add TFTP

---
+++ Design Pattern/Singleton/src/edu/najah/cap/legcy/protocol/TFTPProtocol.java
@@ -0,0 +1,40 @@
+package edu.najah.cap.legcy.protocol;
+
+public class TFTPProtocol {
+	private static TFTPProtocol instance; 
+	private static boolean connected = false;
+	public boolean isConnected() {
+		return connected;
+	}
+	/**
+	 * 
+	 */
+	private TFTPProtocol() {
+		connected = true;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static TFTPProtocol getTFTPInstance() {
+		if (instance == null) {
+			instance = new TFTPProtocol();
+		}
+		return instance;
+	}
+	
+	public boolean releaseTFTP() {
+		if(instance != null) {
+			instance = null;
+			return true;
+		}
+		return false;
+	}
+	
+	public void sendMessage(String message) {
+		System.out.println("I am sending a " + message + " vi TFTP Protocol");
+		
+	}
+
+}
