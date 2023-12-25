2927b4411c9a6aefdc971ca37156016f57100e3b
Mustafa Assaf <mustafaassaf@cloud.upwork.com>
2023-12-26 00:50:12 +0200

    Add Exception handling

--- UserData/UserData/src/edu/najah/cap/exceptions/Util.java
+++ UserData/UserData/src/edu/najah/cap/exceptions/Util.java
@@ -10,8 +10,11 @@ public class Util {
         if (userName == null || userName.isEmpty()) {
             throw new BadRequestException("User name is required");
         }
-        if (userName.length() < 3) {
-            throw new BadRequestException("User name must be at least 3 characters");
+        if (userName.length() < 4) {
+            throw new BadRequestException("User name must be at least 4 characters");
+        }
+        if (!userName.startsWith("user")) {
+            throw new BadRequestException("User name must start with 'user'");
         }
         if (Instant.now().getEpochSecond() % 3 == 0) {
             throw new SystemBusyException("Something went wrong");
